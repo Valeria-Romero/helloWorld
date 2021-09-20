864d4d946a924a7b186068eab0fdbdad68f678b9
Valeria Romero <valellope@gmail.com>
2021-09-20 12:21:50 -0600

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#Task1
+print("Hello world")
+#Task2
+name ="Valeria"
+print("Hello world", name)
+print("Hello world " + name)
+#Task3
+favoriteNumber = 9
+print("Hello", favoriteNumber)
+print("Hello " + str(favoriteNumber))
+#Task4
+favoriteFood1 = "sushi"
+favoriteFood2= "pizza"
+print("I love to eat {} and {}.".format(favoriteFood1, favoriteFood2))
+print(f"I love to eat {favoriteFood1} and {favoriteFood2}.")
